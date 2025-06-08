95f2e9c9f47c41e50edf311c73dd890e1b40a38f
Indrani <indranisaha468@gmail.com>
2025-06-08 22:21:42 +0530

    Parity checker code in c

---
+++ parity-checker.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<string.h>
+#include<math.h>
+int main(){
+    char string[100];
+    printf("Enter a string\n");
+    gets(string);
+    printf("The inpiut string is:");
+    puts(string);
+    int dl=strlen(string);
+    int count=0;
+    for(int i=0;i<dl;i++)
+    {
+        if(string[i]==1)
+            count++;
+    }
+    printf("Enter the mode of parity: even('e'),odd('o')");
+    char ch;
+    count=count%2;
+    scanf("%c",&ch);
+    switch(ch){
+        case 'o':
+        if(count==0)
+            printf("No error");
+        else printf("Error exist");
+        break;
+        case 'e':
+        if(count==1)
+        printf("No error");
+        else printf("Error exist");
+        break;
+        default:
+            printf("Enter valid parity mode..");
+    }
+    return 0;
+}
